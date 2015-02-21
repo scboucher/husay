00434f8b56cf6dac3a8c19ea49458de8f2fae58a
Samuel Boucher <boucher.samuel.c@gmail.com>
2015-02-20 21:19:13 -0500

    Start testing
    add color, line controle

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 'use strict';
+var husay = require('./lib/husay');
 module.exports = function (str) {
-  console.log(str || 'Rainbow');
+    console.log(str || 'Rainbow');
+    husay.setColors('yellow', 'cyan');
+    console.log(husay.endSay(' DBOT', 'This is a fucking test calis'));
+    console.log(husay.endSay('JOBOT'));
+
 };
