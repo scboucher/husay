75b1a44e75ae7a1e2c1f69c593bf4925480a5300
Samuel Boucher <boucher.samuel.c@gmail.com>
2015-06-09 19:38:54 -0400

    fix little stuff

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
 var husay = require('./lib/husay');
 module.exports = function (str) {
-    console.log(str || 'Rainbow');
+    //console.log(str || 'Rainbow');
     husay.setColors('yellow', 'cyan');
-    console.log(husay.endSay(' DBOT', 'This is a fucking test calis'));
+    console.log(husay.endSay(' DBOT', ' One'));
+    husay.setColors('blue', 'green');
     console.log(husay.endSay('JOBOT'));
 
 };
