973abc2705f98d9d32430b1ff1b922f9cad93bf0
Samuel Boucher <boucher.samuel.c@gmail.com>
2015-06-16 22:00:17 -0400

    missing colons

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 var husay = require('./lib/husay');
 module.exports = function (str, main, accent) {
     //console.log(str || 'Rainbow');
-    main = main || 'yellow'
-    accent = accent || 'cyan'
+    main = main || 'yellow';
+    accent = accent || 'cyan';
     husay.setColors(main, accent);
     console.log(husay.endSay(' DBOT', str));
 };
