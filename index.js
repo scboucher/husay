84e8b31dbab56e17264205a12439c96b015c0e64
Samuel Boucher <boucher.samuel.c@gmail.com>
2015-06-17 20:27:32 -0400

    first release

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 'use strict';
-var husay = require('./lib/husay');
-module.exports = function (str, main, accent) {
+var husay = require(__dirname + '/lib/husay');
+module.exports = function (str, core, accent) {
     //console.log(str || 'Rainbow');
-    main = main || 'yellow';
+
+    core = core || 'yellow';
     accent = accent || 'cyan';
-    husay.setColors(main, accent);
+    husay.setColors(core, accent);
     console.log(husay.endSay(' DBOT', str));
 };
--- test/test.js
+++ test/test.js
@@ -7,9 +7,9 @@ var expect = require('chai').expect;
 
 describe('husay node module', function () {
   it(' tests the namePatser method', function () {
-    expect(parser.nameParser('Hubot')).to.equal('Hubot');
-    expect(parser.nameParser('Hubotdfgdgfdfg')).to.equal('Hubot');
-    expect(parser.nameParser('Hubo')).to.equal('Hubo');
-    expect(parser.firstLine('Hubo')).to.equal('/            Hubo             \\');
+    expect(parser.name('Hubot')).to.equal('Hubot');
+    expect(parser.name('Hubotdfgdgfdfg')).to.equal('Hubot');
+    expect(parser.name('Hubo')).to.equal('Hubo');
+    expect(parser.firstLine('Hubo')).to.equal('            Hubo            ');
   });
 });
